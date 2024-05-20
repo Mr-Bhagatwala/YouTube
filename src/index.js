9195b9fbc7bec7ba3bdcfb7e51d0a10b2403ec58
Kaumilkumar Patni <you@example.com>
2024-05-20 20:31:53 +0530

    add ustils for api response and errors

---
+++ src/index.js
@@ -0,0 +1,22 @@
+// require('dotenv').config({path : './env'});
+// import dotenv from "dotenv";
+import connectDB from "./DB/index.js";
+import express from "express";
+import { app } from "./app.js";
+// dotenv.config({
+//     path : './.env'
+// })
+// const app = express();
+connectDB()
+.then(()=>{
+    app.on("error",(error)=>{
+        console.log("ERRR : ",error);
+        throw error
+    })
+    app.listen(process.env.PORT || 3000,()=>{
+        console.log(`server is runnig at port : ${process.env.PORT || 3000}`);
+    })
+})
+.catch((error)=>{
+    console.log("MONGO-DB connection error" , error);
+})
